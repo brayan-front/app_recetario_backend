f980297dccf14082abc1c6c9c654809cec2bbb98
AnderG-Uni <anderson.guevara01@unicatolica.edu.co>
2025-11-17 21:39:46 -0500

    Delete app_recetario_backend/restaurant-module directory
    Se elimina carpeta ya no necesaria 4

--- app_recetario_backend/restaurant-module/restaurant-module/src/main/java/com/recetario/restaurant/repository/RestaurantRepository.java
+++
@@ -1,6 +0,0 @@
-package com.recetario.restaurant.repository;
-
-import com.recetario.restaurant.model.Restaurant;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RestaurantRepository extends JpaRepository<Restaurant, Long> {}
